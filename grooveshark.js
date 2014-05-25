c4a3a407d640ceb626d6338a498740a274e35f97
Sam Kreter <sakfy6@mail.missouri.edu>
2014-05-25 01:11:46 -0500

    added lot of jquery

--- grooveshark.js
+++ grooveshark.js
@@ -3,11 +3,15 @@ var GS = require('grooveshark-streaming');
 
 module.exports.search = function search(data,callback){
 
-GS.Tinysong.getSongInfo(data[0], data[1], function(err, songInfo) {
+GS.Tinysong.getSongInfo('Save Me', 'Shinedown', function(err, songInfo) {
+	console.log(songInfo);
     GS.Grooveshark.getStreamingUrl(songInfo.SongID, function(err, streamUrl) {
+      console.log(streamUrl);
       songInfo.StreamUrl = streamUrl
       songInfo.val = 0;
       callback(songInfo);
     });
   });
 }
+
+//'Love of My Life', 'Queen'
