c280f6d418ed285ed644d9daf188b5eeaba77287
Sam Kreter <sakfy6@mail.missouri.edu>
2014-05-24 23:08:40 -0500

    fixed indenting

--- app.js
+++ app.js
@@ -37,13 +37,32 @@ var playlist = [];
 
 
 app.post('/add', function(req,res){
-		//var hello = "save me,Shinedown";
-	    var data = req.body.split(",");
-	    console.log(data);
-		grooveshark.search(data,function(data){
-			playlist.pop(data);
+    var data = req.body.search.split(", ");
+    console.log(data);
+	grooveshark.search(data,function(data){
+		playlist.push(data);
     });
 });
+
+app.get('/vote',function(req,res){
+	for(i=0;i<playlist.length;i++){
+		if(playlist[i].SongID == res.query['id']){
+			if(req.query['val'] == 'd'){
+				playlist[i].val--;
+			}
+			if(req.query['val'] == 'u'){
+				playlist[i].val++;
+			}
+		}
+	}
+});
+
+app.get('/playlist',function(req,res){
+	res.send(playlist);
+});
+
+
+
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
